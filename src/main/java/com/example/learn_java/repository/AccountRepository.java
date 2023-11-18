dc1e2a44fc827d2eea03c02fdacaa59e0c69985c
Alex <magiclun@yandex.ru>
2023-11-19 01:03:08 +0300

    Initial commit

--- src/main/java/com/example/learn_java/repository/AccountRepository.java
+++ src/main/java/com/example/learn_java/repository/AccountRepository.java
@@ -3,7 +3,6 @@ package com.example.learn_java.repository;
 import com.example.learn_java.model.Account;
 
 public interface AccountRepository {
-    public static void AccountSave (Account account){
+    public void AccountSave (Account account);
 
-    }
 }
--- src/main/java/com/example/learn_java/repository/AccountRepositoryMemoryImpl.java
+++ src/main/java/com/example/learn_java/repository/AccountRepositoryMemoryImpl.java
@@ -1,9 +1,15 @@
 package com.example.learn_java.repository;
 
 
+import com.example.learn_java.model.Account;
+
 import java.util.HashMap;
 
 public class AccountRepositoryMemoryImpl implements AccountRepository{
     public static HashMap<Integer,String> BaseAccount = new HashMap<>();
 
+    @Override
+    public void AccountSave(Account account) {
+
+    }
 }
